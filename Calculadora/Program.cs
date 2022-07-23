7f53ceca43c75b8d158703c0e35a603cd4354dcf
Cleison Salustiano <73855512+cleisonsilvadavi@users.noreply.github.com>
2022-07-23 13:32:51 -0300

    First Commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Calculadora.Services;
+
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraImp c = new CalculadoraImp();
+            int num1 = 10;
+            int num2 = 5;
+   
+            Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
+        }
+    }
+}
